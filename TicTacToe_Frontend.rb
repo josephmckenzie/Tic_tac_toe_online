78c1c90c22a9e9313a0406d75cd71c34934cade1
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-12-18 16:06:46 -0500

    tictactoe

--- TicTacToe_Frontend.rb
+++ TicTacToe_Frontend.rb
@@ -1,48 +1,38 @@
 require 'sinatra'
-enable :sessions
+
 require_relative "ai.rb"
 require_relative"gameplayers.rb"
 require_relative"gameboard.rb"
 
-ai = Random.new()
-players = Gameplayers.new()
+ai = AI.new()
+
 get '/tictactoe' do
-	
-	erb :OneplayerorTwoplayer, :locals => {:message => "", :board => ""}
+	tyoe= params[:typeofgame]
+	erb :OneplayerorTwoplayer, :locals => {:message => "", :board => ai.play_board.board}
 end
 
 post '/tictactoe' do
-
 gameplayers = Gameplayers.new
-	players.type = params[:gametype]
-	if players.type == "1"
-		erb :howhard, :locals => {:message => "You Chose to play a 1 player game", :board => ""}
+	ai.players.type = params[:gametype]
+	if ai.players.type == "1"
+		erb :howhard, :locals => {:message => "You Chose to play a 1 player game", :board => ai.play_board.board}
 	else
-		erb :marker, :locals => {:message => "You Chose to play a 2 player game", :board => ""}
+		erb :marker, :locals => {:message => "You Chose to play a 2 player game", :board => ai.play_board.board}
 	end
 end
 
-# sessions so that we can use it later on in the posts as well 
-
-get '/marker' do
-	session[:difficulty] = params[:difficulty] 
-		if session[:difficulty] == "easy"
-			erb :marker, :locals => {:message => "Really your gonna play EASY?", :board => ""}
-		elsif session[:difficulty] == "medium"
-			erb :marker, :locals => {:message => "Really your gonna play MEDIUM cool but how about hard?", :board => ""}
-		else session[:difficulty] == "negamax"
-			erb :marker, :locals => {:message => "Really your gonna play HARD? You know it can't be beat right?", :board => ""}
-
-		end
+post '/marker' do
+		ai.level = params[:difficulty] if ai.players.type == "1"
+	
+	erb :marker, :locals => {:message => "Really your gonna play that level?", :board => ai.play_board.board}
 end
 
 post '/squares' do
 	ai.players.player1 = params[:XorO]
 	ai.players.player2 = ai.players.p2()
-
-		erb :squares, :locals => {:player1 => ai.players.player1, :player2 => ai.players.player2, 
-							  :message => "Player 1 is #{ai.players.player1} so Player 2 is #{ai.players.player2} ", :message2 => "You AI is a #{session[:difficulty]} level", 
-							  :current => ai.players.current, :board => "", :type => ai.players.type, :invaild => ""}
+	erb :squares, :locals => {:player1 => ai.players.player1, :player2 => ai.players.player2, 
+							  :message => "Player 1 is #{ai.players.player1} so Player 2 is #{ai.players.player2} ", :message2 => "", 
+							  :current => ai.players.current, :board => ai.play_board.board, :type => ai.players.type, :invaild => ""}
 end
 
 post '/game' do
@@ -59,9 +49,6 @@ post '/game' do
 	end	
 end
 
-# i believe this is where we will have to do our if else either that or make a get for each difficulty level i think for the cpu and the status ones man 
-
-
 get '/cpu' do
 	player_marker = ai.players.current_player()
 	move = ai.computer_move(ai.level)
@@ -75,9 +62,9 @@ get '/status' do
 	elsif ai.play_board.board_full?() == true
 		redirect to('/tie')
 	end
-# where is the "1" in ai.players.type coming from? i think this is where start 	our if else to pick our level class 
-ai.players.current = ai.players.change()
-	redirect to('/cpu') if session[:difficulty] == "easy" && ai.players.current == 2
+	
+	ai.players.current = ai.players.change()
+	redirect to('/cpu') if ai.players.type == "1" && ai.players.current == 2
 	erb :squares, :locals => {:player1 => ai.players.player1, :player2 => ai.players.player2, 
 							  :invaild => "", :message => "", :message2 => "", 
 							  :current => ai.players.current, :board => ai.play_board.board, :type => ai.players.type}	
@@ -92,7 +79,8 @@ get '/tie' do
 end
 
 post '/new' do
-	ai = Random.new()
-	
+	ai = AI.new()
+	# game_board = Board.new()
+	# players = Players.new()
 	redirect to('/tictactoe')
 end
